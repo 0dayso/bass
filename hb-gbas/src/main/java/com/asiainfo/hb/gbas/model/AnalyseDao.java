8c9f27b177e11e3fcdc93aab63de46fe3069da9c
AsiainfoXiaohui <xiaohui@asiainfo.com>
2017-08-09 17:13:56 +0800

    完成单个指标分析、模板保存与管理、首页展示模板

--- hb-gbas/src/main/java/com/asiainfo/hb/gbas/model/AnalyseDao.java
+++ hb-gbas/src/main/java/com/asiainfo/hb/gbas/model/AnalyseDao.java
@@ -1,14 +1,21 @@
 package com.asiainfo.hb.gbas.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
 import com.asiainfo.hb.web.models.CommonDao;
 
 @Repository
 public class AnalyseDao extends CommonDao{
 	
+	/**
+	 * 获取所有的gbas配置
+	 * @return
+	 */
 	public Map<String, Object> getGbasList(){
 		String zbSql = "select zb_code code, zb_name name, cycle from gbas.zb_def";
 		String ruleSql = "select rule_code code, rule_name name, cycle from gbas.rule_def";
@@ -19,6 +26,14 @@ public class AnalyseDao extends CommonDao{
 		return resMap;
 	}
 	
+	/**
+	 * 获取多个gbas值
+	 * @param cycle
+	 * @param startTime
+	 * @param endTime
+	 * @param gbasCodes
+	 * @return
+	 */
 	public List<Map<String, Object>> getGbasData(String cycle, String startTime, String endTime, String gbasCodes){
 		String tableName = "monthly".equals(cycle) ? "BOI_TOTAL_monthly" : "BOI_TOTAL_DAILY";
 		StringBuffer sql = new StringBuffer();
@@ -27,6 +42,31 @@ public class AnalyseDao extends CommonDao{
 		return this.jdbcTemplate.queryForList(sql.toString());
 	}
 	
+	/**
+	 * 获取单个指标gbas_val,gbas_val1
+	 * @param cycle
+	 * @param startTime
+	 * @param endTime
+	 * @param gbasCode
+	 * @return
+	 */
+	public List<Map<String, Object>> getOneGbasData(String cycle, String startTime, String endTime, String gbasCode){
+		String tableName = "monthly".equals(cycle) ? "BOI_TOTAL_monthly" : "BOI_TOTAL_DAILY";
+		String condition = " where time_id between " + startTime + " and " + endTime 
+				+ " and gbas_code='" + gbasCode + "'";
+		StringBuffer strBuf = new StringBuffer();
+		strBuf.append("select time_id, gbas_val val, 'gbas_val' name from gbas.").append(tableName).append(condition);
+		strBuf.append(" union all ");
+		strBuf.append("select time_id, gbas_val1 val, 'gbas_val1' name from gbas.").append(tableName).append(condition);
+		return this.jdbcTemplate.queryForList(strBuf.toString());
+	}
+	
+	/**
+	 * 根据gbas_code查询名称
+	 * @param gbasCode
+	 * @param type
+	 * @return
+	 */
 	public String getGbasNameByCode(String gbasCode, String type){
 		String sql = "";
 		if("rule".equals(type)){
@@ -40,6 +80,67 @@ public class AnalyseDao extends CommonDao{
 			name = (String) map.get("name");
 		}
 		return name;
+	}
+	
+	/**
+	 * 保存模板
+	 * @param name
+	 * @param cycle
+	 * @param type
+	 * @param gbasCodes
+	 * @param isFirstPageShow
+	 * @param userId
+	 */
+	public void saveTemplate(String name, String cycle,String type, String gbasCodes,String isFirstPageShow, String userId){
+		String idSql = "select value(max(id), 0) from gbas.analyse_template";
+		int id = this.jdbcTemplate.queryForObject(idSql, Integer.class) + 1;
+		List<String> sqlList = new ArrayList<String>();
+		sqlList.add("insert into gbas.analyse_template (id, name, cycle, type, is_first_page_show, creater_id) values (" 
+				+ id + ",'" + name + "', '" + cycle + "','" + type + "','" + isFirstPageShow + "','" + userId + "')");
+		String[] gbasCodeArr = gbasCodes.split(",");
+		for(String gbasCode: gbasCodeArr){
+			sqlList.add("insert into gbas.analyse_template_ext (id, GBAS_CODE) values (" + id + ",'" + gbasCode + "')");
+		}
+		this.jdbcTemplate.batchUpdate(sqlList.toArray(new String[sqlList.size()]));
+	}
+	
+	/**
+	 * 查询模板信息
+	 * @param userId
+	 * @param name
+	 * @return
+	 */
+	public List<Map<String, Object>> getTemplate(String userId, String name){
+		String sql = "select id, name, cycle, type, is_first_page_show, to_char(create_time,'YYYY-MM-DD HH24:MI:SS') create_time " +
+				" from gbas.ANALYSE_TEMPLATE where creater_id='" + userId + "'";
+		if(!StringUtils.isEmpty(name)){
+			sql += " and name like '%" + name + "%'";
+		}
+		sql += " order by create_time desc";
+		return this.jdbcTemplate.queryForList(sql);
+	}
+	
+	public List<Map<String, Object>> getTemplateExt(String userId, String cycle){
+		String sql = "select name,codes, type from gbas.analyse_template a left join " +
+				" (select id, replace(replace(xml2clob(xmlagg(xmlelement(NAME a, gbas_code))),'<A>',''),'</A>',',') codes " +
+				" from gbas.analyse_template_ext group by id) b " +
+				" on a.id=b.id " +
+				" where a.creater_id='" + userId + "' and cycle='" + cycle + "' and is_first_page_show='1'" +
+				" order by create_time desc " +
+				" FETCH FIRST 6 ROWS ONLY with ur";
+		return this.jdbcTemplate.queryForList(sql);
+	}
+	
+	public void updateTemplate(String ids, String firstPageShow){
+		String sql = "update gbas.ANALYSE_TEMPLATE set is_first_page_show='" + firstPageShow + "' where id in (" + ids + ")";
+		this.jdbcTemplate.update(sql);
+	}
+	
+	public void deleteTemplate(String ids){
+		String sql = "delete from gbas.ANALYSE_TEMPLATE where id in (" + ids + ")";
+		String extSql = "delete from gbas.ANALYSE_TEMPLATE_ext where id in (" + ids + ")";
+		this.jdbcTemplate.update(sql);
+		this.jdbcTemplate.update(extSql);
 	}
 	
 }
